773fc08c63bb02978d78e675f295140b82fc7662
FrancescoCina <francesco.cina04@gmail.com>
2021-09-22 16:54:53 +0200

    Creato e collegato foglio di stile CSS e collegato link Bootstrap

--- index.php
+++ index.php
@@ -34,6 +34,8 @@ $page_contents = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Google FAQ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <?php 
@@ -41,9 +43,7 @@ $page_contents = [
         foreach ($page_content as $tag){
             echo $tag;
         }
-    }
-    
-    
+    }    
     ?>
 </body>
 </html>
